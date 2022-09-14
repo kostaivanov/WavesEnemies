ecf47c89e5c20a57d5ff219451a9ab8c9a090aa4
Kosta Ivanov <kostasvetlozarov@gmail.com>
2022-09-15 01:16:48 +0300

    Update SkillsController.cs, Anthill.unity, and default-2021.dwlt

--- WavesEnemies/Assets/Code/Player/SkillsController.cs
+++ WavesEnemies/Assets/Code/Player/SkillsController.cs
@@ -7,12 +7,13 @@ public class SkillsController : MonoBehaviour
 {
     internal List<PlacePlatformHandler> placePlatformButtons;
     [SerializeField] private List<GameObject> platformPrefab;
+    [SerializeField] private LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-         placePlatformButtons = new List<PlacePlatformHandler>();
-         GameObject.FindGameObjectsWithTag("PlacePlatform").ToList().ForEach(p => placePlatformButtons.Add(p.GetComponent<PlacePlatformHandler>()));
+        placePlatformButtons = new List<PlacePlatformHandler>();
+        GameObject.FindGameObjectsWithTag("PlacePlatform").ToList().ForEach(p => placePlatformButtons.Add(p.GetComponent<PlacePlatformHandler>()));
     }
 
     // Update is called once per frame
@@ -29,10 +30,33 @@ public class SkillsController : MonoBehaviour
                     if (name.StartsWith("H"))
                     {
                         GameObject platform = Instantiate(platformPrefab[0], this.transform.position, Quaternion.identity);
+                        platform.GetComponent<SpriteRenderer>().enabled = false;
+                        bool isTouchingGround = Physics2D.IsTouchingLayers(platform.GetComponent<BoxCollider2D>(), groundLayer);
+                        Debug.Log("is touching = " + isTouchingGround);
+                        if (!isTouchingGround)
+                        {
+                            platform.GetComponent<SpriteRenderer>().enabled = true;
+                        }
+                        else
+                        {
+                            Destroy(platform);
+                        }
+
                     }
                     else
                     {
                         GameObject platform = Instantiate(platformPrefab[1], this.transform.position, Quaternion.identity);
+                        platform.GetComponent<SpriteRenderer>().enabled = false;
+                        bool isTouchingGround = Physics2D.IsTouchingLayers(platform.GetComponent<BoxCollider2D>(), groundLayer);
+
+                        if (!isTouchingGround)
+                        {
+                            platform.GetComponent<SpriteRenderer>().enabled = true;
+                        }
+                        else
+                        {
+                            Destroy(platform);
+                        }
                     }
                 }
             }
